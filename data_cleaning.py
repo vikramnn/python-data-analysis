cbf12be73b9d0cf6fb6194f180e21ec25450404d
Vikram Nagarajan <nagarajan@berkeley.edu>
2023-08-18 15:45:09 -0700

    Initial commit of my python data analysis files. So far, I only have general purpose data cleaning (data_cleaning.py) and specific functions for analyzing thermal conductivity data (kxx_calib.py). I am wondering how I should include my org-roam notes with babel blocks.

---
+++ data_cleaning.py
@@ -0,0 +1,430 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+def getPrefixMultiplier(num):
+    if type(num) == str:
+        return '', 1
+    if num == 0:
+        return '',1
+    elif num < 0:
+        return '',1
+    else:
+        power = np.floor(np.log10(num))
+        power = power-power%3
+        if power == -12:
+            return 'p',1e12
+        if power == -9:
+            return 'n',1e9
+        if power == -6:
+            return 'u',1e6
+        if power == -3:
+            return 'm',1e3
+        if power == 0:
+            return '',1
+        if power == 3:
+            return 'k',1e-3
+        if power == 6:
+            return 'M',1e-6
+
+def getUnits(param_string):
+    split = param_string.split()
+    if 'AC' in param_string:
+        return 'AC'
+    else:
+        if split[1] == '(Hz)':
+            return 'Hz'
+        return split[1][1]
+
+def iterable(obj):
+    try:
+        iter(obj)
+    except Exception:
+        return False
+    else:
+        return True
+
+def sepZFC_FC(df):
+    x = df['Temperature (K)'].values
+    idx = np.argmax(np.abs(np.diff(x)))
+    df_ZFC = df[:idx+1]
+    df_FC = df[idx+1:]
+
+    sep_df = {'ZFC': df_ZFC, 'FC': df_FC}
+    return sep_df
+
+def initialize(time_series):
+    time_series -= time_series.min()
+    return
+
+#Hampel filter to remove outliers
+def hamp_filt(vals_orig, k=21, t0=3):
+    '''
+    vals: pandas series of values from which to remove outliers
+    k: size of window (including the sample; 21 is equal to 10 on either side of value)
+    '''
+
+    #Make copy so original not edited
+    vals = vals_orig.copy()
+
+    #Hampel Filter
+    L = 1.4826
+    rolling_median = vals.rolling(window=k, center=True).median()
+    MAD = lambda x: np.median(np.abs(x - np.median(x)))
+    rolling_MAD = vals.rolling(window=k, center=True).apply(MAD)
+    threshold = t0 * L * rolling_MAD
+    difference = np.abs(vals - rolling_median)
+
+    '''
+    Perhaps a condition should be added here in the case that the threshold value
+    is 0.0; maybe do not mark as outlier. MAD may be 0.0 without the original values
+    being equal. See differences between MAD vs SDV.
+    '''
+
+    outlier_idx = difference > threshold
+    vals[outlier_idx] = np.nan
+    return(vals)
+
+def makeTitle(key, units, dev, ylabel):
+    constr_title = ''
+    if not iterable(key):
+        constr_title = '({}{}) '.format(key,units[0])
+    else:
+        constr_title = '('
+        for i, constr, unit in zip(range(len(key)),key,units):
+            if unit == 'AC':
+                constr_title += '{0}{1}'.format(constr, unit)
+            else:
+                constr_title += '{0:g}{1}'.format(constr * getPrefixMultiplier(constr)[1],getPrefixMultiplier(constr)[0] + unit)
+            if i != len(key)-1:
+                constr_title += ','
+        constr_title += ') '
+
+    if 'X' in dev[0] or 'Y' in dev[0] or 'R' in dev[0]:
+        ylabel = 'V (V)'
+    elif 'Theta' in dev[0] or 'theta' in dev[0]:
+        ylabel = 'Phase (deg)'
+    elif 'res' in dev[0]:
+        ylabel = 'R ($\Omega$)'
+
+    return constr_title, ylabel
+
+def separateSweeps(input_df, sweep_dir='up', sweep_select='up', sep=True, keep_both=False):
+    '''
+    If you have overlapping data, where one set is an up sweep and the other is a down sweep,
+    this function will help separate that
+    '''
+    names = input_df.index.names
+    input_df = input_df.reset_index(level=names[:-1]).drop(columns=names[:-1])
+
+    if sep:
+        if sweep_dir == 'up':
+            sep_index = input_df.index.max()
+            df_up = input_df[:sep_index]
+            df_down = input_df[sep_index:]
+        elif sweep_dir == 'down':
+            sep_index = input_df.index.min()
+
+            df_down = input_df[:sep_index]
+            df_up = input_df[sep_index:]
+        else:
+            raise ValueError
+        sep_df = {'up':df_up, 'down':df_down}
+        if not keep_both:
+            return sep_df[sweep_select]
+        else:
+            df_up['sweep_dir'] = 'up'
+            df_down['sweep_dir'] = 'down'
+            return pd.concat([df_up,df_down])
+    else:
+        output_df = input_df.sort_index()
+        return output_df
+
+def average(input_df, group_titles, thresh=50, tc=30):
+    '''
+    This function average data at a fixed number of data points (given by the innermost_constr axis)
+    For instance, if sitting at certain angles collecting data for some time, this will group
+    the data by angle and average. It will do this for time > tc.
+    '''
+
+    innermost_constr = group_titles[-1]
+
+    input_df = input_df.groupby(group_titles).filter(lambda x: len(x.index)>thresh)
+
+    input_df['Time (ms)'] = input_df.groupby(group_titles).transform(lambda x: x-x.min())['Time (ms)']
+
+    output_df = input_df[input_df['Time (ms)'] > tc*1000].groupby(group_titles).mean()
+
+    return output_df
+
+def get_group_levels(input_df):
+    levels = []
+    for i in range(len(input_df.index.names)-1):
+        levels.append(input_df.index.unique(i).values[0])
+
+    return levels
+
+def subtractOffset(input_df, devices):
+    '''
+    This is for eliminating the offset for the data by subtracting off the minimum indexed value (absolute)
+    e.g. the data at temperature=0 or field=0
+    '''
+    levels = get_group_levels(input_df)
+
+    idx = np.argmin(np.abs(input_df.index.get_level_values(-1).values)) 
+
+    input_df[devices] = input_df[devices].transform(lambda x: x-input_df[devices].loc[(*levels,)].iloc[idx])
+
+    output_df = input_df
+
+    return output_df
+
+def percentOffset(input_df, devices):
+    '''
+    This is for plotting Δ(quantity)/quantity
+    '''
+    levels = get_group_levels(input_df)
+
+    idx = np.argmin(np.abs(input_df.index.get_level_values(-1).values)) 
+
+    input_df[devices] = input_df[devices].transform(lambda x: (x-input_df[devices].loc[(*levels,)].iloc[idx])/input_df[devices].loc[(*levels,)].iloc[idx])
+
+    output_df = input_df
+
+    return output_df
+
+def divideOffset(input_df, devices):
+    '''
+    This is for dividing the data by the minimum indexed value (absolute)
+    e.g. the data at temperature=0 or field=0
+    '''
+    levels = get_group_levels(input_df)
+
+    idx = np.argmin(np.abs(input_df.index.get_level_values(-1).values))   
+
+    input_df[devices] = input_df[devices].transform(lambda x: x/input_df[devices].loc[(*levels,)].iloc[idx])
+
+    output_df = input_df
+
+    return output_df
+
+def subtractXmax(input_df, devices):
+    '''
+    This is for subtracting the data by the maximum indexed value (absolute) (e.g. where field = 9T)
+    '''
+    levels = get_group_levels(input_df)
+
+    idx = np.argmax(np.abs(input_df.index.get_level_values(-1).values))
+
+    input_df[devices] = input_df[devices].transform(lambda x: x-input_df[devices].loc[(*levels,)].iloc[idx])
+
+    output_df = input_df
+
+    return output_df
+
+def subtractYmin(input_df, devices):
+    '''
+    This is for subtracting the data by the minimum in the set (not absolute) 
+    '''
+    input_df[devices] = input_df[devices].transform(lambda x: x - x.min())
+    return input_df
+
+def subtractAverage(input_df, devices):
+    input_df[devices] = input_df[devices].transform(lambda x: x - x.mean())
+    return input_df
+
+def offsetData(input_gb, offset, devices):
+    '''
+    This is for offsetting the different data curves
+    Need to pipe this through groupby since knowledge of all the groups is necessary; 
+    each group gets different offset
+    '''
+    dfs = []
+    for i, group in enumerate(input_gb.groups.keys()):
+        df_group = input_gb.get_group(group)
+        df_group[devices] = df_group[devices].apply(lambda x: x + offset*i)
+        dfs.append(df_group)
+    return pd.concat(dfs)
+
+def subtractOffsetg(input_gb, devices):
+    '''
+    This is for eliminating the offset for the data by subtracting off the minimum indexed value (absolute)
+    e.g. the data at temperature=0 or field=0
+    '''
+    for group in input_gb.groups.keys():
+        df_group = input_gb.get_group(group)
+        idx = np.min(np.abs(df_group.index.get_level_values(-1)))
+        print(df_group[devices].loc[(group,idx)])
+
+def subtractOffset_deprec(input_df, devices):
+    '''
+    This is for eliminating the offset for the data by subtracting off the minimum indexed value (absolute)
+    e.g. the data at temperature=0 or field=0
+    '''
+    names = input_df.index.names
+    input_df.reset_index(inplace=True)
+    idx = input_df[names[-1]].abs().idxmin()
+
+    output_df = (input_df.transform({k: (lambda x, k=k: x - input_df[k].loc[idx]) if k in devices 
+                                    else (lambda x: x) for k in input_df.columns})
+                         .set_index(names)         
+                )
+
+    return output_df
+
+def slice_inner(input_df, start=None, end=None):
+    '''
+    This function slices a dataframe on the highest level (i.e. innermost) index
+    '''
+    slices = []
+    for i in range(len(input_df.index.names)-1):
+        slices.append(slice(None,None,None))
+
+    return input_df.loc[(*slices,slice(start,end)),:]
+
+def unitize_groups(input_df, group_titles):
+    input_df.reset_index(group_titles, inplace=True)
+    for title in group_titles[:-1]:
+        if '(' not in title:
+            units = ''
+        else:
+            units = title.split()[1].split('(')[1][0]
+        input_df[title] = input_df[title].transform(lambda x: '{0}{1}'.format(x*getPrefixMultiplier(x)[1],getPrefixMultiplier(x)[0]+units))
+    output_df = input_df.set_index(group_titles)
+    return output_df
+
+def unitize_title_prefix(group_key, group_titles):
+    units = []
+    if not iterable(group_key):
+        group_key = [group_key]
+    for title in group_titles:
+        if '(' not in title:
+            units.append('')
+        else:
+            units.append(title.split()[1].split('(')[1][0])
+    group_str = ",".join(f"{x*getPrefixMultiplier(x)[1]}{getPrefixMultiplier(x)[0]}{y}" for x,y in zip(group_key,units))
+    group_str = '(' + group_str + ')'
+    return group_str
+
+def unitize_label(label, label_name):
+    if '(' not in label_name:
+        unit = ''
+    else:
+        unit = label_name.split()[1].split('(')[1][0]
+    label_str = '{0}{1}'.format(label*getPrefixMultiplier(label)[1],getPrefixMultiplier(label)[0]+unit)
+    return label_str
+
+def plotdf(input_gb, device_info, group_titles, cmap=plt.get_cmap('magma'), plot_sweeps=False, **kwargs):
+    for gb in input_gb:
+        fig, axes = plt.subplots(len(device_info), 1)
+        key = gb[0]; df_group = gb[1]
+        if not iterable(axes):
+            axes = [axes]
+        for i, dev, ax in zip(range(len(device_info)),device_info,axes):
+            subdf_group = df_group.loc[key]
+            title = unitize_title_prefix(key, group_titles[:-2]) + ' ' + dev[1]
+            num_colors = subdf_group.index.get_level_values(0).unique().size
+            ax.set(title=title, ylabel=dev[2])
+
+            if not plot_sweeps:
+                ax.set_prop_cycle(color=[cmap(1.*k/num_colors) for k in range(num_colors)])
+
+                # for idx in df_group.index.get_level_values(0).unique():
+                #     sub_df_group = df_group.loc[idx]
+                #     sub_df_group[dev[0]].plot(ax=ax, label=idx)
+                # if i == 0:
+                #     ax.legend(ncol=3, fontsize='small')
+
+
+
+                labels = subdf_group.index.get_level_values(0).unique()
+                label_name = subdf_group.index.names[0]
+                labels = list(map(lambda x: unitize_label(x, label_name), labels))
+                (subdf_group.reset_index(label_name)
+                            .groupby(label_name)[dev[0]]
+                            .plot(ax=ax, x=subdf_group.index.get_level_values(1), **kwargs))
+                if i == 0:
+                    ax.legend(labels, loc='best',ncol=3, fontsize='small')
+
+            else:
+                ax.set_prop_cycle(color=[cmap(1.*k/num_colors) for k in range(num_colors)])
+
+                # for idx in df_group.index.get_level_values(0).unique():
+                #     sub_df_group = df_group.loc[idx]
+                #     sub_df_group[dev[0]].plot(ax=ax, label=idx)
+                # if i == 0:
+                #     ax.legend(ncol=3, fontsize='small')
+
+
+
+                labels = subdf_group.index.get_level_values(0).unique()
+                label_name = subdf_group.index.names[0]
+                labels = list(map(lambda x: unitize_label(x, label_name), labels))
+                (subdf_group.reset_index(label_name)
+                            .groupby(label_name)[dev[0]]
+                            .plot(ax=ax, x=subdf_group.index.get_level_values(1), **kwargs))
+                if i == 0:
+                    ax.legend(labels, loc='best',ncol=3, fontsize='small')
+
+            scaleAxes(ax)
+
+    fig.tight_layout()
+
+def plotdf_noGroup(input_df, device_info, group_titles, cmap=plt.get_cmap('magma'), scale=True, **kwargs):
+    fig, axes = plt.subplots(len(device_info), 1)
+    if not iterable(axes):
+        axes = [axes]
+    for i, dev, ax in zip(range(len(device_info)),device_info,axes):
+        title = dev[1]
+        num_colors = input_df.index.get_level_values(0).unique().size
+        ax.set(title=title, ylabel=dev[2])
+        ax.set_prop_cycle(color=[cmap(1.*k/num_colors) for k in range(num_colors)])
+
+        labels = input_df.index.get_level_values(0).unique()
+        label_name = input_df.index.names[0]
+        labels = list(map(lambda x: unitize_label(x, label_name), labels))
+        (input_df.reset_index(label_name)
+                 .groupby(label_name)[dev[0]]
+                 .plot(ax=ax, x=input_df.index.get_level_values(1), marker='o', **kwargs))
+        if i == 0:
+            ax.legend(labels, loc='best',ncol=3, fontsize='small')
+
+        if scale:
+            scaleAxes(ax)
+
+    fig.tight_layout()
+
+def makePlot(fig, ax, title='', xlabel='', ylabel='', cmap=plt.get_cmap('magma'), num_colors=1, xlim=None, ylim=None):
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    if xlim != None:
+        ax.set_xlim(xlim)
+    if ylim != None:
+        ax.set_ylim(ylim)
+    ax.set_prop_cycle(color=[cmap(1.*k/num_colors) for k in range(num_colors)])
+    fig.tight_layout
+
+def scaleAxes(ax):
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+    xmax = max(abs(xmin), abs(xmax))
+    ymax = max(abs(ymin), abs(ymax))
+    xprefix, xmult = getPrefixMultiplier(xmax)
+    yprefix, ymult = getPrefixMultiplier(ymax)
+    if '(' in ax.get_xlabel():
+        paren_prefix = '({}'.format(xprefix)
+        new_label = paren_prefix.join(ax.get_xlabel().split('('))
+        ax.set_xlabel(new_label)
+        ticks_x = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x*xmult))
+        ax.xaxis.set_major_formatter(ticks_x)
+    if '(' in ax.get_ylabel():
+        paren_prefix = '({}'.format(yprefix)
+        new_label = paren_prefix.join(ax.get_ylabel().split('('))
+        ax.set_ylabel(new_label)
+        ticks_y = ticker.FuncFormatter(lambda y, pos: '{0:g}'.format(y*ymult))
+        ax.yaxis.set_major_formatter(ticks_y)
+
+def test():
+    print('hello')
---
+++ kxx_calib.py
@@ -0,0 +1,301 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.polynomial.chebyshev import Chebyshev, chebval
+from numpy.polynomial.polynomial import polyfit, polyval
+from scipy.interpolate import interp1d
+
+def plotCalib(df_calib, thermometers):
+    #thermometers is a list of columns in the dataframe that need to be calibrated
+    fig, axes = plt.subplots(len(thermometers),1)
+    fields = np.unique(df_calib['Field (T)'].values)
+    num_colors = len(fields)
+    cmap = plt.get_cmap('viridis')
+
+    for ax in axes:
+       ax.set_prop_cycle(color=[cmap(1.*k/num_colors) for k in range(num_colors)]) 
+
+    df_avg = (df_calib.groupby(['Field (T)','Temp_round (K)'], as_index=False).filter(lambda x: len(x.index)>50)
+               .groupby(['Field (T)','Temp_round (K)'], as_index=False).mean())
+
+    for i, therm in enumerate(thermometers):
+         df_avg.set_index(therm).groupby('Field (T)')['Temperature (K)'].plot(ax=axes[i], linewidth=0, marker='o', ylabel='Temperature (K)', xlabel='{} ($\Omega$)'.format(therm))
+    
+    axes[0].legend(list(map(lambda x: '{}T'.format(x), fields)), ncol=3)
+    fig.tight_layout()
+    
+    return df_avg
+
+def minMaxInterp(df_interp, poly_order, fields):
+    
+    fig, ((axp_mx, axm_mx), (axp_mn, axm_mn)) = plt.subplots(2,2)
+
+    df_interp.groupby('Field (T)').max().plot(ax=axp_mx, y='R+', linewidth=0, marker='o')
+    df_interp.groupby('Field (T)').max().plot(ax=axm_mx, y='R-', linewidth=0, marker='o')
+    df_interp.groupby('Field (T)').min().plot(ax=axp_mn, y='R+', linewidth=0, marker='o')
+    df_interp.groupby('Field (T)').min().plot(ax=axm_mn, y='R-', linewidth=0, marker='o')
+
+    rp_max = df_interp.groupby('Field (T)').max()['R+'].values
+    rp_min = df_interp.groupby('Field (T)').min()['R+'].values
+    rm_max = df_interp.groupby('Field (T)').max()['R-'].values
+    rm_min = df_interp.groupby('Field (T)').min()['R-'].values
+
+    rp_max_poly = polyfit(fields, rp_max, poly_order)
+    fit = polyval(np.linspace(fields[0], fields[-1], 100), rp_max_poly)
+    axp_mx.plot(np.linspace(fields[0], fields[-1], 100), fit)
+
+    rp_min_poly = polyfit(fields, rp_min, poly_order)
+    fit = polyval(np.linspace(fields[0], fields[-1], 100), rp_min_poly)
+    axp_mn.plot(np.linspace(fields[0], fields[-1], 100), fit)
+
+    rm_max_poly = polyfit(fields, rm_max, poly_order)
+    fit = polyval(np.linspace(fields[0], fields[-1], 100), rm_max_poly)
+    axm_mx.plot(np.linspace(fields[0], fields[-1], 100), fit)
+
+    rm_min_poly = polyfit(fields, rm_min, poly_order)
+    fit = polyval(np.linspace(fields[0], fields[-1], 100), rm_min_poly)
+    axm_mn.plot(np.linspace(fields[0], fields[-1], 100), fit)
+    
+    fig.tight_layout()
+    
+    return rp_max_poly, rp_min_poly, rm_max_poly, rm_min_poly
+
+def minMaxInterpSpline(df_interp, fields, thermometers, kind='cubic'):
+    fig, axes = plt.subplots(len(thermometers),2)
+    x_eval = np.linspace(fields[0], fields[-1], 100)
+
+    domain_interp_dict = {}
+
+    for i,therm in enumerate(thermometers):
+        df_interp.groupby('Field (T)').max()[therm].plot(ax=axes[i,0], y=therm, linewidth=0, marker='o')
+        df_interp.groupby('Field (T)').min()[therm].plot(ax=axes[i,1], y=therm, linewidth=0, marker='o')
+
+        rmax = df_interp.groupby('Field (T)').max()[therm].values
+        rmin = df_interp.groupby('Field (T)').min()[therm].values
+
+        if len(fields) > 1:
+
+            f_interp_max = interp1d(fields, rmax, kind=kind)
+            axes[i,0].plot(x_eval, f_interp_max(x_eval))
+        
+            f_interp_min = interp1d(fields, rmin, kind=kind)
+            axes[i,1].plot(x_eval, f_interp_min(x_eval))
+
+            domain_interp_dict[therm] = [f_interp_max, f_interp_min]
+
+    
+    fig.tight_layout()
+    
+    return domain_interp_dict
+
+def chebyCalibFields(df_avg, cheby_deg, fields, thermometers):
+
+    coeffs_dict = {}
+    
+    plot_dim = int(np.ceil(np.sqrt(len(fields))))
+
+    for therm in thermometers:
+        coeffs_dict[therm] = []
+        
+        fig, axes = plt.subplots(plot_dim, plot_dim)
+
+        if plot_dim != 1:
+            axes = axes.flatten()
+
+        for i,field in enumerate(fields):
+            df_slice = df_avg[df_avg['Field (T)'] == field]
+            R = df_slice[therm].values
+            temperature = df_slice['Temperature (K)'].values
+
+            domain_R = np.log10(np.array([R.min(), R.max()]))
+
+            x = np.linspace(-1,1,100)
+
+            #Fitting
+            coeffs = Chebyshev.fit(np.log10(R), np.log10(temperature), cheby_deg, domain=domain_R).coef
+            
+            coeffs_dict[therm].append(coeffs)
+            
+            z = (x*(domain_R[1] - domain_R[0]) + domain_R[0] + domain_R[1])/2
+            
+            if plot_dim != 1:
+                ax = axes[i]
+            else:
+                ax = axes
+                
+            ax.plot(R, temperature, linewidth=0, marker='o')
+            ax.plot(10**z, 10**chebval(x, coeffs))
+            ax.set_ylabel('T (K)'); ax.set_xlabel('{} ($\Omega$)'.format(therm))
+            ax.set_title('{} T'.format(field));
+
+        fig.tight_layout()
+        
+    return coeffs_dict
+
+
+    # cp = []
+    # cm = []
+    
+    
+    # plot_dim = int(np.ceil(np.sqrt(len(fields))))
+
+        
+    # fig_p, axes_p = plt.subplots(plot_dim, plot_dim)
+    # fig_m, axes_m = plt.subplots(plot_dim, plot_dim)
+        
+    # if plot_dim != 1:
+    #     axes_p = axes_p.flatten(); axes_m = axes_m.flatten()
+    
+    # for i,field in enumerate(fields):
+    #     df_slice = df_avg[df_avg['Field (T)'] == field]
+    #     Rp = df_slice['R+'].values
+    #     Rm = df_slice['R-'].values
+    #     temperature = df_slice['Temperature (K)'].values
+
+    #     domain_Rp = np.log10(np.array([Rp.min(), Rp.max()]))
+    #     domain_Rm = np.log10(np.array([Rm.min(), Rm.max()]))
+
+    #     x = np.linspace(-1,1,100)
+
+    #     #Fitting T+
+    #     coeffs = Chebyshev.fit(np.log10(Rp), np.log10(temperature), cheby_deg, domain=domain_Rp).coef
+    #     cp.append(coeffs)
+    #     z = (x*(domain_Rp[1] - domain_Rp[0]) + domain_Rp[0] + domain_Rp[1])/2
+    #     if plot_dim != 1:
+    #         ax_Rp = axes_p[i]
+    #     else:
+    #         ax_Rp = axes_p
+    #     ax_Rp.plot(Rp, temperature, linewidth=0, marker='o')
+    #     ax_Rp.plot(10**z, 10**chebval(x, coeffs))
+    #     ax_Rp.set_ylabel('T (K)'); ax_Rp.set_xlabel('R+ ($\Omega$)')
+    #     ax_Rp.set_title('{} T'.format(field))
+
+    #     #Fitting T-
+    #     coeffs = Chebyshev.fit(np.log10(Rm), np.log10(temperature), cheby_deg, domain=domain_Rm).coef
+    #     cm.append(coeffs)
+    #     z = (x*(domain_Rm[1] - domain_Rm[0]) + domain_Rm[0] + domain_Rm[1])/2
+    #     if plot_dim != 1:
+    #         ax_Rm = axes_m[i]
+    #     else:
+    #         ax_Rm = axes_m
+    #     ax_Rm.plot(Rm, temperature, linewidth=0, marker='o')
+    #     ax_Rm.plot(10**z, 10**chebval(x, coeffs))
+    #     ax_Rm.set_ylabel('T (K)'); ax_Rm.set_xlabel('R- ($\Omega$)')
+    #     ax_Rm.set_title('{} T'.format(field))
+
+    # fig_p.tight_layout(); fig_m.tight_layout()
+        
+    # return cp, cm
+
+def interpChebyPoly(cp, cm, poly_order, fields):
+    cheby_poly_coefs_p = np.array([])
+    cheby_poly_coefs_m = np.array([])
+    plot_dim = int(np.ceil(np.sqrt(cheby_deg)))
+    fig_p, axes_p = plt.subplots(plot_dim, plot_dim)
+    fig_m, axes_m = plt.subplots(plot_dim, plot_dim)
+    axes_p = axes_p.flatten(); axes_m = axes_m.flatten()
+
+    for i in range(cheby_deg+1):
+        ax = axes_p[i]
+        ax.set_title('c{}+'.format(i)); ax.set_xlabel('Field (T)')
+
+        cp_i = np.array([c[i] for c in cp])
+        poly_coefs_p = polyfit(fields, cp_i, poly_order)
+        fit = polyval(np.linspace(fields[0], fields[-1], 100), poly_coefs_p)
+        cheby_poly_coefs_p = np.append(cheby_poly_coefs_p, poly_coefs_p)
+
+        ax.plot(fields, cp_i, marker='o', linewidth=0, label='c{}'.format(i))
+        ax.plot(np.linspace(fields[0], fields[-1], 100), fit)
+        fig_p.tight_layout()
+
+        ax = axes_m[i]
+        ax.set_title('c{}-'.format(i)); ax.set_xlabel('Field (T)')
+
+        cm_i = np.array([c[i] for c in cm])
+        poly_coefs_m = polyfit(fields, cm_i, poly_order)
+        fit = polyval(np.linspace(fields[0], fields[-1], 100), poly_coefs_m)
+        cheby_poly_coefs_m = np.append(cheby_poly_coefs_m, poly_coefs_m)
+
+        ax.plot(fields, cm_i, marker='o', linewidth=0, label='c{}'.format(i))
+        ax.plot(np.linspace(fields[0], fields[-1], 100), fit)
+        fig_m.tight_layout()
+
+    cheby_poly_coefs_p = cheby_poly_coefs_p.reshape(cheby_deg+1,poly_order+1).T
+    cheby_poly_coefs_m = cheby_poly_coefs_m.reshape(cheby_deg+1,poly_order+1).T
+    
+    return cheby_poly_coefs_p, cheby_poly_coefs_m
+
+def interpChebyPolySpline1D(cp, cm, cheby_deg, fields, kind='cubic'):
+    #lists of interpolations functions for each cheby coeff
+    #Functions are evaluated at a particular field
+    cheby_interp_p = []
+    cheby_interp_m = []
+    x_eval = np.linspace(fields[0], fields[-1], 100)
+    plot_dim = int(np.ceil(np.sqrt(cheby_deg+1)))
+    fig_p, axes_p = plt.subplots(plot_dim, plot_dim)
+    fig_m, axes_m = plt.subplots(plot_dim, plot_dim)
+    axes_p = axes_p.flatten(); axes_m = axes_m.flatten()
+
+    for i in range(cheby_deg+1):
+        ax = axes_p[i]
+        ax.set_title('c{}+'.format(i)); ax.set_xlabel('Field (T)')
+
+        cp_i = np.array([c[i] for c in cp])
+        f_interp = interp1d(fields, cp_i, kind=kind)
+        cheby_interp_p.append(f_interp)
+
+        ax.plot(fields, cp_i, marker='o', linewidth=0, label='c{}'.format(i))
+        ax.plot(x_eval, f_interp(x_eval))
+
+        ax = axes_m[i]
+        ax.set_title('c{}-'.format(i)); ax.set_xlabel('Field (T)')
+
+        cm_i = np.array([c[i] for c in cm])
+        f_interp = interp1d(fields, cm_i, kind=kind)
+        cheby_interp_m.append(f_interp)
+
+        ax.plot(fields, cm_i, marker='o', linewidth=0, label='c{}'.format(i))
+        ax.plot(x_eval, f_interp(x_eval))
+        
+    fig_p.tight_layout(); fig_m.tight_layout()
+    
+    return cheby_interp_p, cheby_interp_m
+
+def extractTemp(R, r_max, r_min, cheby_coefs):
+    logR = np.log10(R)
+    domain = np.log10([r_min, r_max])
+    
+    X = ((logR - domain[0]) - (domain[1] - logR))/(domain[1]-domain[0])
+    T = 10**chebval(X, cheby_coefs)
+    return T
+
+def extractTempSpline(R, B, f_interp_max, f_interp_min, cheby_interp_funcs):
+    cheby_coefs_eval = np.array([f(B) for f in cheby_interp_funcs])
+    logR = np.log10(R)
+    r_min = f_interp_min(B)
+    r_max = f_interp_max(B)
+    domain = np.log10([r_min, r_max])
+    
+    X = ((logR - domain[0]) - (domain[1] - logR))/(domain[1]-domain[0])
+    T = 10**chebval(X, cheby_coefs_eval)
+    return T
+    
+def extractTempPoly(R, B, r_max_poly, r_min_poly, cheby_poly_coefs):
+    '''
+    B is fixed, R is an array, domain is [log10(R_lower), log10(R_upper)]
+    cheby_poly_coefs is a matrix of coefficient values at different fields
+    e.g. c0(B) = c00 + c01*B + ... +c0M * B**M
+         ...
+         [[c00 c10 ... cM0
+            .           .
+           c0M c1M ... cMM]]
+    '''
+    cheby_coefs_eval = polyval(B, cheby_poly_coefs) # This gives a row array of the cheby coefficients at field B
+    logR = np.log10(R)
+    r_min = polyval(B, r_min_poly)
+    r_max = polyval(B, r_max_poly)
+    domain = np.log10([r_min, r_max])
+    
+    X = ((logR - domain[0]) - (domain[1] - logR))/(domain[1]-domain[0])
+    T = 10**chebval(X, cheby_coefs_eval)
+    return T
+
